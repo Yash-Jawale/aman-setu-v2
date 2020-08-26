e0ad81523d6bf68449047e2b2a3b5127c24f636f
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-08-26 13:17:55 +0530

    Completed Calendar

--- js/school-calendar.js
+++ js/school-calendar.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 	var calendarEl = document.getElementById("calendar");
 	var calendar = new FullCalendar.Calendar(calendarEl, {
-		initialView: "dayGridMonth",
+		// initialView: "dayGridMonth",
+		initialView: window.innerWidth < 1024 ? "listMonth" : "dayGridMonth",
 		googleCalendarApiKey: "AIzaSyCT4-bkoT85eQcLKamwQaI8wOYuVtNY3kU",
 		events: {
 			googleCalendarId: "a2689m4pdj6smoi74po8a22sp4@group.calendar.google.com"
+		},
+		eventClick: function (info) {
+			info.jsEvent.preventDefault();
 		}
 		// eventColor: getComputedStyle(document.documentElement).getPropertyValue(
 		// 	"--primary"
 		// )
 	});
 	calendar.render();
+
+	// if (screen.width < 1024) {
+	// 	calendar.changeView("listMonth");
+	// }
 });
